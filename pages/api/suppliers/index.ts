81989b8e7f24d9e972cf7888bfa88d471147d3d0
Viola <pumpure@yahoo.com>
2025-03-22 23:39:57 -0600

    Suppliers table GET POST added

---
+++ pages/api/suppliers/index.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import supabase from "../../../lib/supabase";
+
+export default async function handler(req: NextApiRequest, 
+    res: NextApiResponse) {
+    if (req.method === "GET") {
+        const {data, error} = await supabase.from("suppliers").select("*");
+        if (error) {
+            return res.status(500).json({ error: error.message});
+        }
+        return res.status(200).json(data);
+    } else if (req.method === "POST") {
+        const { supplier_name, contact_person, phone, email, address, notes } = req.body;
+        const { data, error} = await supabase
+        .from("suppliers")
+        .insert([
+            { supplier_name, contact_person, phone, email, address, notes}
+        ]);
+        if (error) {
+            return res.status(500).json({ error: error.message});
+        }
+        return res.status(201).json(data);
+    } else {
+        res.setHeader("Allow", ["GET", "POST"]);
+        return res.status(405).end(`Method ${req.method} Not Allowed`)
+    }
+}
